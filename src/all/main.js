0df06ddd5dd4c23c0eadadda5448e46a234b0c1d
aleph ramos <105456824+alephsramos@users.noreply.github.com>
2024-09-20 10:07:14 -0300

    as

--- src/all/main.js
+++ src/all/main.js
@@ -27,28 +27,30 @@ navBallImg.src = '../../assets/img/logomarca/fast_solo.svg'; // Insira o link da
 navBall.appendChild(navBallImg);
 document.body.appendChild(navBall);
 
-// Esconde a bolinha inicialmente
+// Esconde a bolinha e a navbar inicialmente
+navBall.style.transform = 'translateX(-50%) scale(0)';
 navBall.style.opacity = '0';
-navBall.style.pointerEvents = 'none';
+navbar.style.transform = 'scale(1)';
+navbar.style.opacity = '1';
 
 window.addEventListener('scroll', function() {
     // Quando rolar, faz a transição suave para esconder a navbar e mostrar a bolinha
-    navbar.style.opacity = '0';
+    navbar.style.transform = 'scale(0)'; // Esconde a navbar
+    navbar.style.opacity = '0'; // Fica invisível
     navbar.style.pointerEvents = 'none'; // Desabilita cliques enquanto está escondida
-    navBall.style.opacity = '1';
-    navBall.style.pointerEvents = 'auto'; // Habilita cliques na bolinha
+    navBall.style.transform = 'translateX(-50%) scale(1)'; // Mostra a bolinha
+    navBall.style.opacity = '1'; // Fica visível
 
     // Limpa o timeout para reiniciar a contagem de tempo
     clearTimeout(timeout);
 
     // Configura um timeout para mostrar a navbar novamente após parar de rolar
     timeout = setTimeout(function() {
-        navbar.style.opacity = '1';
+        navbar.style.transform = 'scale(1)'; // Mostra a navbar novamente
+        navbar.style.opacity = '1'; // Fica visível
         navbar.style.pointerEvents = 'auto'; // Habilita cliques novamente
-        navBall.style.opacity = '0'; // Esconde a bolinha
-        navBall.style.pointerEvents = 'none';
+        navBall.style.transform = 'translateX(-50%) scale(0)'; // Esconde a bolinha
+        navBall.style.opacity = '0'; // Fica invisível
     }, 1000); // Ajuste o tempo que deseja para a navbar voltar após parar a rolagem
 });
-
-
 
