061fae6564dca28ffead439a0c52284d314f07a6
aleph ramos <105456824+alephsramos@users.noreply.github.com>
2024-10-02 14:31:14 -0300

    Update animate.js

--- src/scripts/animate.js
+++ src/scripts/animate.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const animateElements = document.querySelectorAll('.animate');
-  
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('in-view');
-        } else {
+  const animateElements = document.querySelectorAll('.animate');
+
+  // Função para verificar se o usuário está em um dispositivo iPhone
+  function isIPhone() {
+    return /iPhone/i.test(navigator.userAgent);
+  }
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+
+        // Verifica se é iPhone
+        if (isIPhone()) {
+          observer.unobserve(entry.target); // Remove o elemento do observador somente em iPhones
+        }
+      } else {
+        // Somente remove a animação se não for iPhone
+        if (!isIPhone()) {
           entry.target.classList.remove('in-view');
         }
-      });
-    }, { threshold: 0.1 }); // Ajuste o valor do threshold conforme necessário
-  
-    animateElements.forEach(element => {
-      observer.observe(element);
+      }
     });
+  }, { threshold: 0.1 }); // Ajuste o valor do threshold conforme necessário
+
+  animateElements.forEach(element => {
+    observer.observe(element);
   });
+});
